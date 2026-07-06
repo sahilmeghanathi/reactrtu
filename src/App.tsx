c728feaa74f512ab58eee4cd97f993fcd8af92a6
sahilgiri.meghanathi <sahilgiri.meghanathi@simformsolutions.com>
2026-07-06 15:23:26 +0530

    Added Basic files and folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { test } from "@/utils/test";
+
 export default function App() {
+  console.log("Test:", test);
   return (
     <div className="flex h-screen items-center justify-center bg-red-800">
-      <h1 className=" font-bold text-yellow-300">
-        Tailwind CSS is working!
-      </h1>
+      <h1 className=" font-bold text-yellow-300">Tailwind CSS is working!</h1>
     </div>
   );
 }
