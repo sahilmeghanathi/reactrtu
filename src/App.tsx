896b42e747d887f8af3e4f80ea3224c7e13722fb
sahilgiri.meghanathi <sahilgiri.meghanathi@simformsolutions.com>
2026-07-06 14:28:44 +0530

    Initial React Boilerplate Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-red-800">
+      <h1 className=" font-bold text-yellow-300">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+  );
+}
