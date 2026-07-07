922fccccd031918c763db0ee9e83ba0acd1af0ee
sahilgiri.meghanathi <sahilgiri.meghanathi@simformsolutions.com>
2026-07-07 15:54:06 +0530

    Feat : Basic auth pages

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { ROUTES } from "./route-paths";
 
 export default function ProtectedRoute() {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   return isAuthenticated ? <Outlet /> : <Navigate replace to={ROUTES.LOGIN} />;
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,10 @@ import MainLayout from "@/layouts/MainLayout";
 import AuthLayout from "@/layouts/AuthLayout";
 import DashboardLayout from "@/layouts/DashboardLayout";
 
-import Home from "@/pages/Home/Home";
-import Login from "@/pages/Login/Login";
-import Dashboard from "@/pages/Dashboard/Dashboard";
-import NotFound from "@/pages/NotFound/NotFound";
+import Home from "@/pages/Website/Home/Home";
+import Login from "@/pages/Auth/login/Login";
+import Dashboard from "@/pages/WebPanel/Dashboard/Dashboard";
+import NotFound from "@/pages/common/NotFound/NotFound";
 
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
@@ -16,9 +16,9 @@ import { ROUTES } from "./route-paths";
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
+      {/* <Route element={<MainLayout />}>
         <Route path={ROUTES.HOME} element={<Home />} />
-      </Route>
+      </Route> */}
 
       <Route element={<PublicRoute />}>
         <Route element={<AuthLayout />}>
